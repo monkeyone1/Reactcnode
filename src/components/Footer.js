ec528be18a578ae11f0773a1f26bcdf5e9b17d00
monkeyone1 <a793472281@qq.com>
2017-04-13 21:11:04 +0800

    完成头尾

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import q from '../images/q.png';
+import u from '../images/u.png';
+import ali from '../images/ali.png';
 
 class Footer extends Component {
   render() {
@@ -8,7 +11,8 @@ class Footer extends Component {
     }
     return (
       <div style={{
-        background: '#fff'
+        background: '#fff',
+        padding:'10px'
       }}>
 
         <div
@@ -29,8 +33,8 @@ class Footer extends Component {
           </div>
           <p style={styles.p}>CNode 社区为国内最专业的 Node.js
              开源技术社区，致力于 Node.js 的技术研究。</p>
-          <p style={styles.p}>服务器赞助商为
-            存储赞助商为  七牛云存储 ，由  alinode 提供应用性能服务</p>
+          <p style={styles.p}>服务器赞助商为<img style={{width:'92px'}} src={u} alt="img"/>
+            存储赞助商为 <img style={{width:'115px'}} src={q} alt="img"/> ，由 <img style={{width:'166px'}}src={ali} alt=""/> 提供应用性能服务</p>
 
             <p style={styles.p}>新手搭建 Node.js 服务器，
               推荐使用无需备案的 <a href="https://www.digitalocean.com/?refcode=eba02656eeb3">DigitalOcean(https://www.digitalocean.com/)</a></p>
