aa528bc8fe3fb19794e64123bb1b0eb51d7b25c1
monkeyone1 <a793472281@qq.com>
2017-04-13 14:44:31 +0800

    完成评论栏

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -1,34 +1,47 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import axios from 'axios';
+import Replies from './Replies.js';
 import '../css/topic.css'
 class Topic extends Component {
   constructor() {
     super();
     this.state={
-      data:[]
+      data:[],
+      wait:false
     }
   }
     componentWillMount() {
      axios.get(`https://cnodejs.org/api/v1/topic/${this.props.params.id}`)
      .then((res) => {
        this.setState({
-         data:res.data.data
+         data:res.data.data,
+          wait:true
        });
      })
      
    }
   render() {
-    console.log(this.props.params.id);
+    console.log(this.state.data);
 
     return (
-      <div>
+      
       <div style={{
        marginRight:'305px',
-       backgroundColor:'#fff',
-       borderRadius: '3px 3px 0 0'
-      }}>
+      
+       
+      }} >
+      <div style={{
+        backgroundColor:'#fff',
+         borderRadius: '3px',
+         marginBottom:'13px'
+      }} >
         <div dangerouslySetInnerHTML={{__html:this.state.data.content}} className='container'/>
       </div>
+
+            {
+             this.state.wait? <Replies replies={this.state.data.replies} />: ''      
+              
+            } 
       </div>
     );
   }
